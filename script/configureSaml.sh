f8bb1055117548482d05d4fbf90b6da4f803f7ae
Hans Hörberg <hans.horberg@umu.se>
2013-11-11 10:32:48 +0100

    ...

--- script/configureSaml.sh
+++ script/configureSaml.sh
@@ -8,7 +8,8 @@ if [ ! -d "$1" ]; then
   echo "usage: setupLinux.sh pysaml2_path"
   exit
 fi
-pysaml2Path=$1
+basePath=$1
+pysaml2Path="$1/pysaml2"
 echo "Do you want to configure an test IdP and SP? (Y/n):"
 read CONFIGUREIDPSP
 if [ $CONFIGUREIDPSP = "Y" ]
@@ -19,9 +20,9 @@ then
   spMetadataFile="$pysaml2Path/example/sp/yaisSp.xml"
   idpMetadataFile="$pysaml2Path/example/idp/yaisIdp.xml"
   echo "IdP setup"
-  sudo setupIdp.py $pysaml2Path /usr/yais/templates/idp/create_testserver_idp_conf.json -M $spMetadataFile
+  sudo setupIdp.py $basePath /usr/yais/templates/idp/create_testserver_idp_conf.json -M $spMetadataFile
   echo "SP setup"
-  sudo setupSp.py $pysaml2Path /usr/yais/templates/sp/create_testclient_sp_conf.json -M idpMetadataFile
+  sudo setupSp.py $basePath /usr/yais/templates/sp/create_testclient_sp_conf.json -M idpMetadataFile
   make_metadata.py $idpConfFile > idpMetadataFile
   make_metadata.py $spConfFile > spMetadataFile
   echo "Do you want to start test IdP and SP? (Y/n):"
