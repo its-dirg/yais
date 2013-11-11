9b7350e250256c7efc3205c894cef4dc0a7a7acc
Hans Hörberg <hans.horberg@umu.se>
2013-11-11 14:22:30 +0100

    ...

--- script/yaisLinux.sh
+++ script/yaisLinux.sh
@@ -4,11 +4,11 @@ INSTALLPYSAML2="n"
 INSTALLBASE="n"
 if [$1 = "-h"]
 then
-    echo "usage: setupLinux.sh install_path"
+    echo "usage: setupLinux.sh install_path [group]"
 fi
 if [ ! -d "$1" ]; then
   echo $1 is not a directory!
-  echo "usage: setupLinux.sh install_path"
+  echo "usage: setupLinux.sh install_path [group]"
   exit
 fi
 basePath = $1
