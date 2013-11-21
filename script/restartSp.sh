3c4da167008d8143cf3c0fa8cb980dc52610c114
Hans Hörberg <hans.horberg@umu.se>
2013-11-21 09:16:45 +0100

    ...

--- script/restartSp.sh
+++ script/restartSp.sh
@@ -10,4 +10,4 @@ if [ ! -d "$1" ]; then
 fi
 stopSp.sh
 cd $1/pysaml2/example/sp
-nohup python sp.py sp_conf > $1/sp.out 2> $1/sp.err < /dev/null &
+nohup python sp.py > $1/sp.out 2> $1/sp.err < /dev/null &
