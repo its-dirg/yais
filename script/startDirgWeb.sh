e36b712852d602010dcb5a2e1e07fff131777b52
Hans Hörberg <hans.horberg@umu.se>
2014-01-10 14:10:27 +0100

    Correction for dirg-web

--- script/startDirgWeb.sh
+++ script/startDirgWeb.sh
@@ -8,7 +8,8 @@ if [ ! -d "$1" ]; then
   echo "usage: startDirgWeb.sh path"
   exit
 fi
-cd $1/saml2testGui
+cd $1/dirg-web
 nohup python dirg_web_server.py server_conf > $1/dirgweb.out 2> $1/dirgweb.err < /dev/null &
 sleep 10
 cat $1/dirgweb.out
+cat $1/dirgweb.err
