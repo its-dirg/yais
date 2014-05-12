530d0d4ff645de30ad2b7dcd22322817a7861065
Hans Hörberg <hans.horberg@umu.se>
2014-05-12 16:11:11 +0200

    Lägger till tomma metadatafiler.

--- script/configureSaml.sh
+++ script/configureSaml.sh
@@ -17,15 +17,17 @@ then
   sudo pip install pyOpenSSL==0.13.1
   idpConfFile="$pysaml2Path/example/idp2/idp_conf.py"
   idpMetadataFile="$pysaml2Path/example/idp2/idp_conf.xml"
-  spConfFile="$pysaml2Path/example/sp/sp_conf.py"
-  spMetadataFile="$pysaml2Path/example/sp/sp_conf.xml"
+  spConfFile="$pysaml2Path/example/sp-repoze/sp_conf.py"
+  spMetadataFile="$pysaml2Path/example/sp-repoze/sp_conf.xml"
+  touch $spMetadataFile
+  touch $idpMetadataFile
   echo "IdP setup"
   setupIdp.py $basePath /usr/yais/templates/idp/create_testserver_idp_conf.json -M $spMetadataFile
   echo "SP setup"
   setupSp.py $basePath /usr/yais/templates/sp/create_testclient_sp_conf.json -M $idpMetadataFile
   cd "$pysaml2Path/example/idp2"
   make_metadata.py idp_conf.py > idp_conf.xml
-  cd "$pysaml2Path/example/sp"
+  cd "$pysaml2Path/example/sp-repoze"
   make_metadata.py sp_conf.py > sp_conf.xml
   echo "Do you want to start test IdP and SP? (Y/n):"
   read STARTCONFIGUREIDPSP
